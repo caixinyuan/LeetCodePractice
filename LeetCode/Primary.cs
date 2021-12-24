385affba6354c2ce28f69debe09316eda6c12837
caixy <318900260@qq.com>
2021-12-24 09:57:01 +0800

    MoveZeroes

--- LeetCode/Primary.cs
+++ LeetCode/Primary.cs
@@ -190,19 +190,32 @@ namespace LeetCode
         /// <param name="nums"></param>
         public void MoveZeroes(int[] nums)
         {
-            for (int i = 0; i < nums.Length - 1; i++)
-            {
-                for (int j = 0; j < nums.Length - 1 - i; j++)
-                {
-                    if (nums[j] == 0)
-                    {
-                        int temp = nums[j];
-                        nums[j] = nums[j + 1];
-                        nums[j + 1] = temp;
-                    }
+            //for (int i = 0; i < nums.Length - 1; i++)
+            //{
+            //    for (int j = 0; j < nums.Length - 1 - i; j++)
+            //    {
+            //        if (nums[j] == 0)
+            //        {
+            //            int temp = nums[j];
+            //            nums[j] = nums[j + 1];
+            //            nums[j + 1] = temp;
+            //        }
 
+            //    }
+            //}
+            int index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[index++] = nums[i];
                 }
             }
+            for (int i = index; i < nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+
         }
 
     }
